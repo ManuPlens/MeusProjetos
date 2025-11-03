0e22d9c73f79dd9e6526a6a883bc6df494b2300d
Emanuelly Plens <plensmanu777@gmail.com>
2025-11-02 23:25:35 -0300

    Exercicio 1
    colocando // para dar a descrição do código

--- exercicio1.c
+++ exercicio1.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+//Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 int main() {
 
   int a, b, c, soma;
