82a1c8d05cf28588ea268d10735201d3bffe80a9
Emanuelly Plens <plensmanu777@gmail.com>
2025-11-02 22:28:02 -0300

    exercicio1

---
+++ exercicio1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+  int a, b, c, soma;
+
+  printf("digite o valor de A:");
+  scanf("%d", &a);
+  printf("digite o valor de B:");
+  scanf("%d", &b);
+  printf("digite o valor de C:");
+  scanf("%d", &c);
+soma = a + b;
+
+printf("soma de a + b: %d\n", soma);
+printf("valor de c: %d\n", c);
+
+if(soma < c) {
+  printf("a soma de a + b eh menor que c");
+  }  else {
+    printf("a soma de a + b eh maior que c");
+  }
+return 0;
+}
