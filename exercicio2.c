bcfd4fc42075c3fa011caa923c09ba59b9c43e92
Emanuelly Plens <plensmanu777@gmail.com>
2025-11-04 06:30:25 -0400

    Exercicio 2
    atividade pré n1

---
+++ exercicio2.c
@@ -0,0 +1,25 @@
+//Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+#include <stdio.h>
+
+int main() {
+int n;
+
+printf("digite um numero:\n");
+scanf("%d", &n);
+
+if(n > 0) {
+  printf("numero eh positivo e", n);
+} else if (n < 0) {
+  printf("numero eh negativo e", n);
+} else {
+  printf("numero eh zero\n", n);
+ return 0;
+}
+
+if (n %2 == 0) {
+  printf(" o numero eh par\n");
+} else {
+  printf(" o numero eh impar\n");
+}
+return 0 ;
+}
